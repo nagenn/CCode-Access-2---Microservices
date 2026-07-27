3eb84576287f83bbca56813c91dd47b6647a2d3f
nagenn <44678680+nagenn@users.noreply.github.com>
2026-07-27 12:52:45 +0530

    Update start.sh to set COMPOSE_PROJECT_NAME

--- contractiq-services/start.sh
+++ contractiq-services/start.sh
@@ -2,6 +2,9 @@
 set -e
 cd "$(dirname "$0")"
 
+PARENT_NAME=$(printf '%s' "$(basename "$(dirname "$(pwd)")")" | tr '[:upper:]' '[:lower:]' | tr -c 'a-z0-9_-' '-')
+export COMPOSE_PROJECT_NAME="contractiq-${PARENT_NAME}"
+
 docker compose up -d --build
 sleep 5
 docker compose ps
